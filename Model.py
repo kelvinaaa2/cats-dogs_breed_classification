c5b93ba803d7112c6569a25487193174ed8dc03d
kelvinaaa2 <kelvin430kahung@gmail.com>
2022-04-25 01:19:58 +0800

    model development

--- Model.py
+++ Model.py
@@ -7,19 +7,18 @@ class BreedClassifier(nn.Module):
 
     def __init__(self, config):
         super().__init__()
-        self.hidden = config['MODEL']['HIDDEN']
 
         # Call Pretrained Network
         self.pretrained = getattr(models, config['MODEL']['PRETRAINED'])(pretrained=True)
-        n_in = next(self.pretrained.fc.modules()).in_features  # Raise except pretrained.classifier for densenet
+        try:
+            n_in = next(self.pretrained.fc.modules()).in_features  # pretrained.fc for resnet
+        except:
+            n_in = next(self.pretrained.classifier.modules()).in_features  # pretrained.classifier for densenet
 
         # Make Classifier
         self.hidden_layers = nn.ModuleList([nn.Linear(n_in, self.hidden)])
+        layer_sizes = zip(config['MODEL']['HIDDEN'][:-1], config['MODEL']['HIDDEN'][1:])
 
     def forward(self):
         pass
-
-
-model = getattr(models, 'resnet50')(pretrained=True)
-print(1)
 
